a86168224afbb7e4f0aaa107e86f6d2760b35f01
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-04-24 04:45:11 +0200

    Added contact section

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+
 import Header from "../components/header/Header";
+import Contact from "./contact/Contact";
 
 export default function Main() {
   return (
     <div>
       <Header />
+      <Contact />
     </div>
   );
 }
